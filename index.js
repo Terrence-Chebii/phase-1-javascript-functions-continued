d9282fe158a990a81ea03cdda0cb253655bb30f5
bigman <terrence.chebii@student.moringaschool.com>
2023-03-15 13:19:53 +0300

    too hard

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity1 = 'roller-skate'){
+    return `This Saturday, I want to ${activity1}!`
+}
+console.log(saturdayFun())
+
+function mondayWork(activity2 = "go to the office"){
+    return `This Monday, I will ${activity2}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(special = 'You are'){
+    const innerFunction = function(place = "Moringa"){
+        return `${special} ${place}`
+    }
+    return innerFunction()
+}
+console.log(wrapAdjective())
